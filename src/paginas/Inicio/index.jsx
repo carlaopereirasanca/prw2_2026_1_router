a4cb0842851889531627d12ebc5cce58ca08131c
Carlos Jose Pereira <carlaopereirasanca@gmail.com>
2026-03-27 11:43:52 -0300

    apos aula 2

--- src/paginas/Inicio/index.jsx
+++ src/paginas/Inicio/index.jsx
@@ -1,8 +1,26 @@
 
+import Post from "@/componentes/Post";
+import styles from './Inicio.module.css';
+import posts from '@/json/posts.json'
+
 export default function Inicio() {
 
+    // Apenas o conteúdo principal da página Inicio,
+    // isto é, os posts !!
+
     return (
-        <h1>Olá Mundo!</h1>
-    );
+
+        <ul className={styles.posts}>
+
+            { posts.map( (post)=> {
+                return (
+                    <li key={post.id}>
+                        <Post post={post} />
+                    </li>
+                );
+            } ) }
+
+        </ul>
+    )
 }
 
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,27 +3,37 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from './paginas/Inicio';
 import SobreMim from './paginas/SobreMim';
 import Menu from "./componentes/Menu";
+import Rodape from "./componentes/Rodape";
+import PaginaPadrao from "./componentes/PaginaPadrao";
 
 function AppRoutes() {
 
-	return (
+    return (
 
-		<BrowserRouter>
+        <BrowserRouter>
 
-      <Menu />
+            { /* Menu fora de todas as rotas, p/ aparecer em todas as páginas */ }
+            <Menu />
 
-			<Routes>
+            <Routes>
 
-				<Route path="/" element={<Inicio />} />
+                <Route path="/" element={ <PaginaPadrao /> }>
 
-				<Route path="/sobremim" element={<SobreMim />} />
+                    <Route path="/" element={<Inicio />} />
+                    <Route path="/sobremim" element={<SobreMim />} />
 
-				<Route path="*" element={<h1>Página não encontrada!</h1>} />
+                </Route>
 
-			</Routes>
+                <Route path="*" element={<h1>Página não encontrada!</h1>} />
 
-		</BrowserRouter>
-	)
+            </Routes>
+
+            { /* Rodape fora de todas as rotas, p/ aparecer em todas as páginas */ }
+            <Rodape />
+
+        </BrowserRouter>
+
+    )
 }
 
 export default AppRoutes;
